53666ac99b795ae8effc3357eb7974fc3721afc4
vinzbongat <66341710+vinzbongat@users.noreply.github.com>
2023-06-02 23:34:33 +0800

    2 files modified

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { Product, products } from '../products';
 
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
   styleUrls: ['./product-details.component.css']
 })
+
 export class ProductDetailsComponent {
 
+  product: Product | undefined;
+  /* ... */
 }
